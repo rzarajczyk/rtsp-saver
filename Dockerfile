8fae3a760bcbc43573b06d057537e4b4a0db76c3
rzarajczyk <rzarajczyk@gmail.com>
2024-03-09 22:28:57 +0100

    apply-limit.sh - first attempt

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,7 @@ WORKDIR /app
 
 COPY . .
 
-RUN apt update && apt install -y ffmpeg
+RUN apt update && apt install -y ffmpeg cron
+RUN crontab -l | { cat; echo "*/15 * * * * bash /app/apply-limit.sh"; } | crontab -
 
 CMD ["/bin/bash", "./src/run.sh"]
---
+++ src/apply-limit.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+exec 2>&1 1>/cronlog.log
+
+echo "$(date --iso=seconds) | Running apply-limit.sh"
+
+cd /output
+
+LIMIT=5000000
+
+SIZE=$(du | tr -dc '0-9')
+while [ "$SIZE" -gt "$LIMIT" ];
+do
+  echo "$(date --iso=seconds) | Space occupied by the recordings: $SIZE bytes is greater than configured limit $LIMIT bytes"
+
+  OLDEST=$(ls -tp | grep -v '/$' | tail -n 1)
+  echo "$(date --iso=seconds) | Removing file $OLDEST"
+  rm "$OLDEST"
+  sleep 1
+
+  SIZE=$(du | tr -dc '0-9')
+done
